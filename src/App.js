cff487d7a8a6bde28cf5b38a1309e38307353e68
Jozivan <francisco.freire@rscinovacao.com.br>
2020-04-02 12:13:24 -0300

    tasks passed to component List

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import React, { Component } from 'react';
    import Tasks from './components/tasks/Tasks';
    import Header from './components/header/Header';
    
-   import { library } from '@fortawesome/fontawesome-svg-core'
-   import { faCheckCircle, faTrashAlt } from '@fortawesome/free-regular-svg-icons'   
-   library.add(faCheckCircle, faTrashAlt)
-   
+  //  import { library } from '@fortawesome/fontagitwesome-svg-core'
+  //  import { faCheckCircle, faTrashAlt } from '@fortawesome/free-regular-svg-icons'   
+  //  library.add(faCheckCircle, faTrashAlt)
+  import { library }  from '@fortawesome/fontawesome-svg-core'
+  import { far } from '@fortawesome/free-regular-svg-icons'
+  import { fas } from '@fortawesome/free-solid-svg-icons'
+  
+  library.add(far, fas)
+  
    class App extends Component {
      render() {
        return (
