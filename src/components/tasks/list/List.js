300295d972532ee007510da4ffa7c9291e54a884
Jozivan <francisco.freire@rscinovacao.com.br>
2020-04-02 14:42:27 -0300

    [c] create_tasks.js

--- src/components/tasks/list/List.js
+++ src/components/tasks/list/List.js
@@ -5,6 +5,23 @@ import React, { Component } from 'react';
    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
    
    class List extends Component {
+      async checkTask(task) {
+        let form = {'task': {'done': 'true'}}
+        await fetch(`http://localhost:3001/tasks/${task.id}`,
+          {
+            method: 'PUT',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              task: { done: true } 
+            })
+          }
+        )
+        
+        this.props.loadTasks();
+      }
       async deleteTask(task) {
         if (window.confirm(`Are you sure you want to delete: "${task.title}"`)) {
           await fetch(`http://localhost:3001/tasks/${task.id}`, {method: 'DELETE'});
@@ -22,13 +39,13 @@ import React, { Component } from 'react';
                    return <tr key={task.id}>
                      <td className="col-md-10">{task.title}</td>
                      <td>
-                       { 
-                         task.done == false
-                         ? <a className="check" href="#">
-                             <FontAwesomeIcon icon="check-circle"/>
-                           </a> 
-                         : null
-                       }
+                      { 
+                        task.done == false
+                          ? <a className="check" href="#">
+                            <FontAwesomeIcon icon="check-circle" onClick={() => this.checkTask(task)} size="lg"/>
+                          </a> 
+                          : null
+                      }
                      </td>
                      <td>
                       <a className="delete" href="#" onClick={() => this.deleteTask(task)}>
